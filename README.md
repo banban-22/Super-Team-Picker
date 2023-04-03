1923990224df8ef971e82d608c50b447589743c0
AyakaB <ayaka.bando22@gmail.com>
2023-04-03 12:53:59 -0700

    fixed required field

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Super-Team-Picker
+
+## What are the feature of your software?
+
+1.
+2.
+3.
+
+## The technologies that has been used in your software?
+
+1. express - popular and powerful web application framework for Node.js
+   ⋅⋅1. a fast and lightweight framework that allows you to build web applications quickly and efficiently
+   ⋅⋅2. provides a simple way to define routes for your web application. You can define different routes for different HTTP methods (such as GET, POST, PUT, DELETE) and handle the requests and responses accordingly.
+   ⋅⋅3. It is highly scalable and can handle large volumes of traffic without slowing down or crashing
+   ⋅⋅4. It has a large and active community of developers who contribute to its development and provide support for other users
+2. knex with PostgreSQL (pg)
+   ⋅⋅1. allows easy migration between different databases, so you can switch from PostgreSQL to another database in the future, you can easily do so without having to rewrite your entire codebase.
+   ⋅⋅2. PG has high-performance capabilities, especially when dealing with large datasets. Knex allows you to write optimized SQL queries that take advantage of PostgreSQL's performance capabilities.
+3. morgan - Morgan is a middleware for Node.js that provides logging capabilities for HTTP requests and responses.
+   ⋅⋅1. makes easy to debug your web application by logging detailed information about incoming requests and outgoing responses.
+   ⋅⋅2. provides a range of customization options that allow you to control what information is logged and how it is formatted. e.g. you can log additional information such as headers, status codes, and response times.
+   ⋅⋅3. can be used to track the performance of your web application by logging request and response times.
+4. ejs
+   ⋅⋅1. allowing you to generate dynamic HTML pages (with JavaScript code) within your Express.js application.
+   ⋅⋅2. You can create template with EJS. This helps reduce code duplication and makes it easier to maintain your codebase.
+
+## What are the advantages for using your software?
+
+1. We can divide members as much as fairly into the given team numbers / members per team
+2. Since we can store members name and team names into database, we can easily take their information out
+3. You are also able to edit and delete team members and team names easily
+4. We are using simple design, so you can easily know how to use it
--- routes/cohorts.js
+++ routes/cohorts.js
@@ -1,10 +1,8 @@
-
 const express = require('express');
 const db = require('../db/client');
 const router = express.Router();
-const url = require('url')
+const url = require('url');
 const querystring = require('querystring');
-
 
 // --------------- INDEX ----------------
 router.get('/', (request, response) => {
@@ -29,6 +27,12 @@ router.post('/new', (request, response) => {
   console.log(request.body);
   const { logo_url, name_of_team, name_of_members } = request.body;
 
+  if (name_of_team === '' || name_of_members === '') {
+    response
+      .status(400)
+      .render('error', { err: 'Please fill in required fields' });
+  }
+
   db('teams')
     .insert({ logo_url, name_of_team, name_of_members })
     .then((data) => {
@@ -41,121 +45,114 @@ router.post('/new', (request, response) => {
     });
 });
 
-
-
-router.get('/:id' , (request , response) => {
+router.get('/:id', (request, response) => {
   const { id } = request.params;
   const parsedurl = url.parse(request.url);
   const query = querystring.parse(parsedurl.query);
   let number = query.number;
   db('teams')
-  .where('id' , id)
-  .first()
-  .then(data => {
-    let members = data.name_of_members.split(',');
-    if (number && query.radio){
-      if ( members.length < number){
-        response.send('<h2>The number of teams should be less than the number of members.</h2>');
-      } else {
+    .where('id', id)
+    .first()
+    .then((data) => {
+      let members = data.name_of_members.split(',');
+      if (number && query.radio) {
+        if (members.length < number) {
+          response.send(
+            '<h2>The number of teams should be less than the number of members.</h2>'
+          );
+        } else {
+          let temp = [];
+          let final = [];
 
-        let temp = [];
-        let final = [];
-
-        function split(arr){
-            if (arr.length == 1){
-                temp.push(arr[0]);
+          function split(arr) {
+            if (arr.length == 1) {
+              temp.push(arr[0]);
             } else {
-                let random = Math.floor(Math.random() * arr.length);
-                temp.push(arr[random]);
-                arr.splice(random , 1);
-                split(arr);
+              let random = Math.floor(Math.random() * arr.length);
+              temp.push(arr[random]);
+              arr.splice(random, 1);
+              split(arr);
             }
-        };
-        split(members);
+          }
+          split(members);
 
-        if (query.radio == 'member'){
+          if (query.radio == 'member') {
+            number = Math.ceil(temp.length / number);
+          }
 
-          number = Math.ceil(temp.length/number);
-        
-        };
-
-        for (let i = 0 ; i < number ; i++){
+          for (let i = 0; i < number; i++) {
             final.push([]);
-        };
+          }
 
-        while (temp.length != 0){
-            for (let j = 0 ; j < number ; j++){
-                final[j].push(temp.pop());
-                if (temp.length == 0){
-                    break;
-                }
+          while (temp.length != 0) {
+            for (let j = 0; j < number; j++) {
+              final[j].push(temp.pop());
+              if (temp.length == 0) {
+                break;
+              }
             }
-        };
+          }
 
-        response.render('cohorts/show' , { data , final })
-
-      }      
-    } else {
-      response.render('cohorts/show' , { data , final:'' });
-    }
-  }).catch(err => {
-    console.error(err);
-  })
+          response.render('cohorts/show', { data, final });
+        }
+      } else {
+        response.render('cohorts/show', { data, final: '' });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 });
 
 // --------------- EDIT TEAM ----------------
 
 router.get('/:id/edit', (request, response) => {
-  const { id } = request.params; 
-   db("teams")
-      .where("id", id)
-      .then(team => {
-          console.log(team)
-          response.render('cohorts/edit', ...team );
-      })
-      .catch(ex => {
-          console.error(ex);
-          response.send("<h1>Something went wrong</h1>")
-      });
-
-})
+  const { id } = request.params;
+  db('teams')
+    .where('id', id)
+    .then((team) => {
+      console.log(team);
+      response.render('cohorts/edit', ...team);
+    })
+    .catch((err) => {
+      console.error(err);
+      response.status(500).render('error', { err });
+    });
+});
 
 router.patch('/:id', (request, response) => {
-  
   let { logo_url, name_of_team, name_of_members } = request.body;
 
   db('teams')
-      .update({
-          logo_url: logo_url,
-          name_of_team: name_of_team,
-          name_of_members : name_of_members 
-      })
-      .where("id", request.params.id)
-      .then(data => {
-          response.redirect("/cohorts/" + request.params.id);
-      })
-      .catch(ex => {
-          console.error(ex);
-          response.send("<h1>Something went wrong</h1>")
-      });
-})
+    .update({
+      logo_url: logo_url,
+      name_of_team: name_of_team,
+      name_of_members: name_of_members,
+    })
+    .where('id', request.params.id)
+    .then((data) => {
+      response.redirect('/cohorts/' + request.params.id);
+    })
+    .catch((err) => {
+      console.error(err);
+      response.status(500).render('error', { err });
+    });
+});
 
 // --------------- DELETE TEAM ----------------
 
-
 router.delete('/:id', (request, response) => {
   //delete post
-  db("teams")
-      .del()
-      .where("id", request.params.id)
-      .then(data => {
-          response.redirect("/cohorts");
-      })
-      .catch(ex => {
-          console.error(ex);
-          response.send("<h1>Something went wrong</h1>")
-      });
-})
-
+  db('teams')
+    .del()
+    .where('id', request.params.id)
+    .then((data) => {
+      response.redirect('/cohorts');
+    })
+    .catch((err) => {
+      console.error(err);
+      response.status(500).render('error', { err });
+    });
+});
 
 module.exports = router;
